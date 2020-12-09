4fb16cab5d5a4ce793d92becd349cf73baacee23
art sturgeon <sturgeon.art@gmail.com>
2020-12-09 15:12:53 +0000

    adding failing test

---
+++ app/src/test/java/org/sturgeon/appcentaur/SomethingTest.kt
@@ -0,0 +1,18 @@
+package org.sturgeon.appcentaur
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+class SomethingTest {
+
+    @Test
+    fun `test that something`() {
+
+        assert("something" == "goat")
+        
+    }
+
+
+
+}
